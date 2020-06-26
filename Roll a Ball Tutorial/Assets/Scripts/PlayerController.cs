ee3ea71ed8263249d226fc7157cecdde5424f725
Mokarrom Rahman <mrahman0000@gmail.com>
2020-06-26 17:44:17 -0600

    Added the pick up objects, walls, and the plater ball.

---
+++ Roll a Ball Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using Packages.Rider.Editor.PostProcessors;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Called before rendering a frame
+    //this is where most of the game code will go
+    //private void Update()
+    //{
+    //don't need this 
+    //}
+
+    public float speed;
+
+    private Rigidbody rb;
+    private void Start()
+    {
+        //initialize the ball rigidbody
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Called before any physics calculations are done
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //We want to move the ball only on the plane
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Roll a Ball Tutorial/Assets/Scripts/Rotator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate the transform 
+        //* Time.deltaTime ensures that the pick up object is 
+        //frame rate independent
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+
+
+        ChangeColorsDynamic(GetComponent<Renderer>());
+
+
+
+
+
+    }
+
+    //Dynamically change colors
+    void ChangeColorsDynamic(Renderer renderer)
+    {
+
+        if (renderer.material.GetColor("_Color") == Color.red)
+        {
+            //Call SetColor using the shader property name "_Color" and setting the color to red
+            renderer.material.SetColor("_Color", Color.yellow);
+        }
+        else
+        {
+            //Call SetColor using the shader property name "_Color" and setting the color to red
+            renderer.material.SetColor("_Color", Color.red);
+        }
+    }
+}
