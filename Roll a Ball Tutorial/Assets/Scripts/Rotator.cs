3aa8f9c4343fa3c6cee01d95e50d06932f9cbc53
Mokarrom Rahman <mrahman0000@gmail.com>
2020-06-29 16:42:39 -0600

    added the changing color based on time. It did not work very well.

--- Roll a Ball Tutorial/Assets/Scripts/Rotator.cs
+++ Roll a Ball Tutorial/Assets/Scripts/Rotator.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using System;
 
 public class Rotator : MonoBehaviour
 {
+
+    private float time = 0.0f;
+    public float interpolationPeriod = 1000.0f;
+
+
     // Update is called once per frame
     void Update()
     {
@@ -12,28 +19,34 @@ public class Rotator : MonoBehaviour
         //frame rate independent
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
 
+        time += Time.deltaTime;
 
+        if (time >= interpolationPeriod)
+        {
+            time = 0.0f;
+
+            // execute block of code here
+            ChangeColorsDynamic(GetComponent<Renderer>());
+            Console.WriteLine("I'm here");
+        }
         //ChangeColorsDynamic(GetComponent<Renderer>());
-
-
-
-
-
     }
 
     //Dynamically change colors
     void ChangeColorsDynamic(Renderer renderer)
     {
-       
-        if (renderer.material.GetColor("_Color") == Color.red)
-        {
-            //Call SetColor using the shader property name "_Color" and setting the color to red
-            renderer.material.SetColor("_Color", Color.yellow);
-        }
-        else
-        {
-            //Call SetColor using the shader property name "_Color" and setting the color to red
-            renderer.material.SetColor("_Color", Color.red);
-        }
+
+            if (renderer.material.GetColor("_Color") == Color.red)
+            {
+                //Call SetColor using the shader property name "_Color" and setting the color to red
+                renderer.material.SetColor("_Color", Color.yellow);
+            }
+            else
+            {
+                //Call SetColor using the shader property name "_Color" and setting the color to red
+                renderer.material.SetColor("_Color", Color.red);
+            }
+        
+
     }
 }
